3496389695072c958876fcd8d21f7de6a2f1330d
AnnaTeremizova <amura780@gmail.com>
2025-01-16 11:29:57 +0300

    data types in java

---
+++ src/main/java/org/example/data_types/App.java
@@ -0,0 +1,26 @@
+package org.example.data_types;
+
+public class App {
+    public static void main(String[] args) {
+        // Примитивные типы данных
+        // Целочисленные типы данных
+        int myInt = 10; // -2³¹ до 2³¹-1. (4 байта)
+        short myShort = 3266; // -32768 до 32767 (2 байта)
+        long myLong = 22445632; // -2⁶³ до 2⁶³-1. (8 байт)
+        byte myByte = 127; // -128 до 127. (1 байт)
+
+        // Вещественные типы данных
+        double myDouble = 456.4; // (8 байт)
+        float myFloat = 4545.5f; // (4 байта)
+
+        // Символьный тип данных
+        char myChar = 'a'; // Хранит одиночный символ в формате Unicode (от 0 до 65535), (2 байта)
+
+        // Логический тип данных
+        boolean myBoolean = true; // true false
+
+        // Ссылочные типы данных
+        String s = "Anna";
+        // Строки, интерфейсы, массивы, классы, перечисления, коллекции, потоки (stream) и др.
+    }
+}
