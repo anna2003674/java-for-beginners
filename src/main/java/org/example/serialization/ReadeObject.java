db957a201dd59e00777f769529a9a63122e8bfde
AnnaTeremizova <amura780@gmail.com>
2025-01-21 22:42:40 +0300

    serialization part 1

---
+++ src/main/java/org/example/serialization/ReadeObject.java
@@ -0,0 +1,27 @@
+package org.example.serialization;
+
+import java.io.*;
+
+public class ReadeObject {
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fis = new FileInputStream("people.bin");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Person person1 = (Person) ois.readObject();
+            Person person2 = (Person) ois.readObject();
+
+            System.out.println(person1);
+            System.out.println(person2);
+
+            ois.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
