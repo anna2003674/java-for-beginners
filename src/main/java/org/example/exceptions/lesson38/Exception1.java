40e04fc5b4e84afe114a904d1123723a253dd152
AnnaTeremizova <amura780@gmail.com>
2025-01-21 22:07:21 +0300

    checked and unchecked exception

---
+++ src/main/java/org/example/exceptions/lesson38/Exception1.java
@@ -0,0 +1,17 @@
+package org.example.exceptions.lesson38;
+
+public class Exception1 {
+    public static void main(String[] args) {
+        // Checked Exception (во время компиляции) = исключительные случаи в работе программы (их нужно обрабатывать)
+        // Unchecked Exception (во время выполнения программы) = ошибка в работе программы
+        // (ошибку не нужно обрабатывать, ее нужно исправлять)
+
+        //int a = 1 / 0; // Unchecked Exception
+
+        //String name = null;
+        //name.length(); // Unchecked Exception
+
+        //int arr[] = new int[2];
+        //System.out.println(arr[2]); // Unchecked Exception
+    }
+}
