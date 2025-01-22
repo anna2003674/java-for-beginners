f8f6f8b4a5d9a91877e91417d6351a273c306797
AnnaTeremizova <amura780@gmail.com>
2025-01-22 19:58:11 +0300

    abstract classes

---
+++ src/main/java/org/example/lesson45/Cat.java
@@ -0,0 +1,8 @@
+package org.example.lesson45;
+
+public class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Мяу");
+    }
+}
---
+++ src/main/java/org/example/lesson45/Dog.java
@@ -0,0 +1,8 @@
+package org.example.lesson45;
+
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Гав гав");
+    }
+}
