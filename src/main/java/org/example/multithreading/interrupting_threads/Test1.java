8e6664eb10d31814233f760c41171206b0ce7d55
AnnaTeremizova <amura780@gmail.com>
2025-01-30 01:52:02 +0300

    interrupting threads

---
+++ src/main/java/org/example/multithreading/interrupting_threads/Test1.java
@@ -0,0 +1,27 @@
+package org.example.multithreading.interrupting_threads;
+
+import java.util.Random;
+
+public class Test1 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Random random = new Random();
+                for (int i = 0; i < 1_000_000_000; i++) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println("Thread was interrupted");
+                        break;
+                    }
+                    Math.sin(random.nextDouble());
+                }
+            }
+        });
+        System.out.println("Starting thread");
+        thread1.start();
+        Thread.sleep(1000);
+        thread1.interrupt();
+        thread1.join();
+        System.out.println("Thread has finished");
+    }
+}
