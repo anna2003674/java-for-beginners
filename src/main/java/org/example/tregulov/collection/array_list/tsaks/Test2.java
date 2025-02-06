b149d70caf20b5cb3f3757e2e81772694a014fae
AnnaTeremizova <amura780@gmail.com>
2025-02-06 17:13:45 +0300

    adding and removing elements

---
+++ src/main/java/org/example/tregulov/collection/array_list/tsaks/Test2.java
@@ -0,0 +1,19 @@
+package org.example.tregulov.collection.array_list.tsaks;
+
+import java.util.ArrayList;
+
+/**
+ * Создайте ArrayList<Integer>. Добавьте в него числа от 1 до 10.
+ * Удалите все четные числа. Выведите итоговый список.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+        list.removeIf(n -> n % 2 == 0);
+        System.out.println(list);
+    }
+}
