8adc099589424bc8f3ec86d1ffe49b482be8717f
AnnaTeremizova <amura780@gmail.com>
2025-02-06 17:21:30 +0300

    finding the max and min element

---
+++ src/main/java/org/example/tregulov/collection/array_list/tsaks/Test3.java
@@ -0,0 +1,39 @@
+package org.example.tregulov.collection.array_list.tsaks;
+
+import java.util.ArrayList;
+
+/**
+ * Напишите метод, который находит максимальное и минимальное число в ArrayList<Integer>.
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(40);
+        list.add(4);
+        list.add(40000);
+        list.add(354);
+        list.add(665);
+        minAndMaxElements(list);
+    }
+
+    private static void minAndMaxElements(ArrayList<Integer> arr) {
+        if (arr.isEmpty()) {
+            System.out.println("Список пуст!");
+            return;
+        }
+        Integer minElement = arr.get(0);
+        Integer maxElement = arr.get(0);
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) > maxElement) {
+                maxElement = arr.get(i);
+            }
+            if (arr.get(i) < minElement) {
+                minElement = arr.get(i);
+            }
+        }
+        System.out.println("Минимальный элемент: " + minElement);
+        System.out.println("Максимальный элемент: " + maxElement);
+    }
+}
