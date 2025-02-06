2b5921d09e55474f2e971dfc8f4d90f91a59c561
AnnaTeremizova <amura780@gmail.com>
2025-02-06 17:00:06 +0300

    creating and displaying a list

---
+++ src/main/java/org/example/tregulov/collection/array_list/tsaks/Test1.java
@@ -0,0 +1,21 @@
+package org.example.tregulov.collection.array_list.tsaks;
+
+import java.util.ArrayList;
+
+/**
+ * Создайте ArrayList из 5 строк. Выведите его элементы на экран.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Anna");
+        list.add("Dasha");
+        list.add("Masha");
+        list.add("Alex");
+        list.add("Misha");
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
