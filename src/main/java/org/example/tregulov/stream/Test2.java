625162dce7c431cb687f2abbb54a95c74ae09498
AnnaTeremizova <amura780@gmail.com>
2025-02-10 02:05:32 +0300

    stream method filter

---
+++ src/main/java/org/example/tregulov/stream/Test2.java
@@ -0,0 +1,104 @@
+package org.example.tregulov.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
+        Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'm', 35, 4, 7);
+        Student st5 = new Student("Mariya", 'f', 23, 3, 7.4);
+        List<Student> list = new ArrayList<>();
+        list.add(st1);
+        list.add(st2);
+        list.add(st3);
+        list.add(st4);
+        list.add(st5);
+        list = list.stream().filter(element -> element.getAge() > 22 && element.getAvgGrade() < 7.2)
+            .collect(Collectors.toList());
+        System.out.println(list);
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Andrew", "Charlie");
+        List<String> filteredNames = names.stream()
+            .filter(name -> name.startsWith("A"))
+            .collect(Collectors.toList());
+        System.out.println(filteredNames);
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> evenNumbers = numbers.stream()
+            .filter(n -> n % 2 == 0)
+            .collect(Collectors.toList());
+        System.out.println(evenNumbers);
+    }
+}
+
+class Student {
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "name='" + name + '\'' +
+            ", sex=" + sex +
+            ", age=" + age +
+            ", course=" + course +
+            ", avgGrade=" + avgGrade +
+            '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+}
