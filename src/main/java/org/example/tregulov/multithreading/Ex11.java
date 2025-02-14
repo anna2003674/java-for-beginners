4fa0a6fa10594570c1f6c346f4fe5e4ce7005f6b
AnnaTeremizova <amura780@gmail.com>
2025-02-14 13:42:49 +0300

    data race and synchronized

---
+++ src/main/java/org/example/tregulov/multithreading/Ex11.java
@@ -0,0 +1,29 @@
+package org.example.tregulov.multithreading;
+
+public class Ex11 {
+    static int counter = 0;
+
+    public static synchronized void increment() {
+        counter++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new R());
+        Thread thread2 = new Thread(new R());
+        thread1.start();
+        thread2.start();
+        thread2.join();
+        thread1.join();
+        System.out.println(counter);
+    }
+}
+
+class R implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            Ex11.increment();
+        }
+    }
+}
