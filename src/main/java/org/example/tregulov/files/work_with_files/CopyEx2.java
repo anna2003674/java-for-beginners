6d9b344b458a79eb287ab6eb5ee13f879218ed07
AnnaTeremizova <amura780@gmail.com>
2025-02-18 20:39:56 +0300

    FileInputStream и FileOutputStream

---
+++ src/main/java/org/example/tregulov/files/work_with_files/CopyEx2.java
@@ -0,0 +1,25 @@
+package org.example.tregulov.files.work_with_files;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyEx2 {
+    public static void main(String[] args) {
+        try (FileInputStream inputStream =
+                 new FileInputStream("C:\\Users\\AnnaTeremizova\\Desktop\\code-java.jpg");
+             FileOutputStream outputStream = new FileOutputStream("code-java.jpg");
+        ) {
+            int i;
+            while ((i = inputStream.read()) != -1) {
+                outputStream.write(i);
+            }
+            System.out.println("Done!");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
